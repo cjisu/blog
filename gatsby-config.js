046f27371c617aa43d65b7cd1bf3ac5d5d58f4cd
CHOI jisu <h41469921@gmail.com>
2019-07-09 00:43:59 +0900

    Add gatsby-transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,9 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        path: `${__dirname}/src/content/blog`,
+        path: `${__dirname}/src`,
       },
     },
+    `gatsby-transformer-remark`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,37 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default () => <div>Hello world!</div>
+class BlogIndex extends React.Component {
+  render() {
+    const { data } = this.props
+    const posts = data.allMarkdownRemark.edges
+
+    return (
+        <div>
+            {posts.map(({node}) => {
+                return (
+                  <div>{node.frontmatter.title}</div>
+                )
+            })}
+        </div>
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+              publishedAt
+            }
+          }
+        }
+      }
+  }
+`
